20b7e63509010d3210eb811146b6dbdc4833def3
zahrayousefijamarani <z.y.j.1379@gmail.com>
2020-03-29 18:09:04 +0430

    add suggestion adapter and icon

--- app/src/main/java/edu/sharif/sharif_dev/weather/MainActivity.java
+++ app/src/main/java/edu/sharif/sharif_dev/weather/MainActivity.java
@@ -6,8 +6,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
@@ -15,52 +15,42 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    final ArrayList<String> cityNames = new ArrayList<>();
+    final ArrayList<ArrayList<Double>> centerClasses = new ArrayList<>();
+    ArrayAdapter<String> resultAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 // check internet connection
-        if(!checkConnection()){
-            goToWeatherPage(0,0,"",false);
+        if (!checkConnection()) {
+            goToWeatherPage(0, 0, "", false);
         }
         setContentView(R.layout.activity_main);
 
-// make array adapter
-        final ArrayList<String> cityNames = new ArrayList<>();
+// set array adapter
         final ListView list = findViewById(R.id.listview_search);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.search_list_row, cityNames);
-        list.setAdapter(adapter);
-        final ArrayList<ArrayList<Double>> centerClasses = new ArrayList<>();
+        resultAdapter = new ArrayAdapter<>(this, R.layout.search_list_row, cityNames);
+        list.setAdapter(resultAdapter);
+
+
+        setAutoCompleteInput();
 
         Button search_btn = findViewById(R.id.search_btn);
         search_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startSearching(cityNames,adapter,centerClasses);
-            }
-        });
-
-        AutoCompleteTextView search_input = findViewById(R.id.search_inp);
-        String[] locations = getResources().getStringArray(R.array.location_suggestion);
-        ArrayAdapter<String> searchAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, locations );
-        search_input.setAdapter(searchAdapter);
-        search_input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_ACTION_SEARCH){
-                    startSearching(cityNames,adapter,centerClasses);
-                    handled = true;
-                }
-                return handled;
+                startSearching();
             }
         });
 
@@ -95,21 +85,21 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(getString(R.string.internet_status), internetConnection);
         startActivity(intent);
 // to finish this activity  = not show main activity and use cache
-        if(!internetConnection){
+        if (!internetConnection) {
             finish();
         }
     }
 
-    private boolean checkConnection(){
+    private boolean checkConnection() {
         ConnectivityManager cm =
-                (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
     }
 
-    private void startSearching(ArrayList<String> cityNames,ArrayAdapter<String> adapter,ArrayList<ArrayList<Double>> centerClasses ){
+    private void startSearching() {
         String searchText;
         EditText search_input = findViewById(R.id.search_inp);
         if (search_input.getText() != null & !Objects.requireNonNull(search_input.getText()).toString().equals("")) {
@@ -123,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             builder = builder.withProgressBar(progressBar);
             builder = builder.withContext(getApplicationContext());
             builder = builder.withCityNames(cityNames);
-            builder = builder.withArrayAdapter(adapter);
+            builder = builder.withArrayAdapter(resultAdapter);
             builder = builder.withCenterClasses(centerClasses);
 
             GetMap getMap = builder.build();
@@ -134,6 +124,28 @@ public class MainActivity extends AppCompatActivity {
             CustomHandler ch = new CustomHandler(getApplicationContext());
             ch.sendIntMessage(R.string.search_input_error);
         }
+    }
+
+    private void setAutoCompleteInput() {
+        final AutoCompleteTextView search_input = findViewById(R.id.search_inp);
+        final ArrayList<String> locations = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.location_suggestion)));
+        final SearchSuggestionAdapter searchSuggestionAdapter = new SearchSuggestionAdapter(this, R.layout.suggestion_layout, locations);
+        search_input.setAdapter(searchSuggestionAdapter);
+// defines the number of characters to get before display suggestions
+        search_input.setThreshold(1);
+
+        search_input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    startSearching();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/edu/sharif/sharif_dev/weather/SearchSuggestionAdapter.java
@@ -0,0 +1,102 @@
+package edu.sharif.sharif_dev.weather;
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SearchSuggestionAdapter extends ArrayAdapter<String> {
+    private Context context;
+    private int resourceId;
+    private List<String> items, tempItems, suggestions;
+
+    public SearchSuggestionAdapter(@NonNull Context context, int resourceId, ArrayList<String> items) {
+        super(context, resourceId, items);
+        this.items = items;
+        this.context = context;
+        this.resourceId = resourceId;
+        tempItems = new ArrayList<>(items);
+        suggestions = new ArrayList<>();
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        try {
+            if (convertView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                view = inflater.inflate(resourceId, parent, false);
+            }
+            String suggestion = getItem(position);
+            TextView name = view.findViewById(R.id.suggestion_text);
+            ImageView iconView = view.findViewById(R.id.suggestion_icon);
+            iconView.setImageResource(R.drawable.pointer_blue);
+            name.setText(suggestion);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return view;
+    }
+    @Nullable
+    @Override
+    public String getItem(int position) {
+        return items.get(position);
+    }
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return suggestionFilter;
+    }
+    private Filter suggestionFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            if (charSequence != null) {
+                suggestions.clear();
+// find the suggestion
+                for (String suggestion: tempItems) {
+                    if (suggestion.toLowerCase().startsWith(charSequence.toString().toLowerCase())) {
+                        suggestions.add(suggestion);
+                    }
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = suggestions;
+                filterResults.count = suggestions.size();
+                return filterResults;
+            } else {
+                return new FilterResults();
+            }
+        }
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            ArrayList<String> tempValues = (ArrayList<String>) filterResults.values;
+            if (filterResults != null && filterResults.count > 0) {
+                clear();
+                for (String suggestionObj : tempValues) {
+                    add(suggestionObj);
+                }
+                // notify the adapter
+                notifyDataSetChanged();
+            } else {
+                clear();
+                notifyDataSetChanged();
+            }
+        }
+    };
+}
